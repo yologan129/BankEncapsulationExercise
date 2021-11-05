15b1d13a980dc9d0e6fbc77ed3c77c37aa67bee5
yologan129 <logan128128@gmail.com>
2021-11-05 10:54:12 -0500

    Made new Account

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private readonly double _balance;
+        
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating as Deposit of the {amount, 0:c} to your account!!");
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+
+    
+
+    
+    
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much $$$ do you want to deposit?");
+            double amounttoDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amounttoDeposit);
+            double userbalance = ba.GetBalance();
+
+            Console.WriteLine("Your current balance is: {userBalance, 0:c}");
+        }
+    }
+}
